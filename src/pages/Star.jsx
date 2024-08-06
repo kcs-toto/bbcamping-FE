83bbbd76f74a4023304911dc5f876eb0249e34b6
yzooop <qkrduswn0404@gmail.com>
2024-08-07 02:03:27 +0900

    💄 car css

--- src/pages/Star.jsx
+++ src/pages/Star.jsx
@@ -5,14 +5,14 @@ function Star() {
     return (
         <div className="Star">
             <div className="weather-section">
-                <h2 style={{textAlign: "center", color: "white"}}>오늘은 별보기 좋은 날입니다.</h2>
+                <h2 style={{textAlign: "center", color: "black"}}>오늘은 별보기 좋은 날입니다.</h2>
                 <div className="meta-data">
                     <div className="weather">
-                        <div className="emogi">🌞</div>
+                        <div className="emoji">🌞</div>
                         <div className="contents">25도</div>
                     </div>
                     <div className="season">
-                        <div className="emogi">🌸</div>
+                        <div className="emoji">🌸</div>
                         <div className="contents">봄</div>
                     </div>
                 </div>
