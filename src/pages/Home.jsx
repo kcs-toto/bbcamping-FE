e4c9cc868db50dd28e4ea4cfa42beeefcd8c96d5
yzooop <qkrduswn0404@gmail.com>
2024-08-07 04:30:08 +0900

    ✨ map

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -52,19 +52,7 @@ function Home() {
         <Map/>
         <img src="/path/to/placeholder-image.png" alt="Map placeholder" />
       </div>
-      <form className="form">
-        <button>
-          <svg width="17" height="16" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-labelledby="search">
-            <path d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9" stroke="currentColor" stroke-width="1.333" stroke-linecap="round" stroke-linejoin="round"></path>
-          </svg>
-        </button>
-        <input className="input" placeholder="Search ..." required="" type="text" />
-        <button className="reset" type="reset">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
-          </svg>
-        </button>
-      </form>
+      
       <div className="info-modal">
         <img src={Camping} alt="Camping icon" className="camping-image" />
         <p className="info-message">마커를 클릭해 제주도의 캠핑장과<br />별 스팟을 확인해보세요!</p>
